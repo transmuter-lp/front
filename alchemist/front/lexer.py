b76aefe28905f81497c46c88fd0953dbaa480f92
natanjunges <natanajunges@gmail.com>
2021-10-14 22:47:01 -0300

    Replace the identifier processing code with a user defined default handler in lexer_run

--- alchemist/front/lexer.py
+++ alchemist/front/lexer.py
@@ -17,9 +17,9 @@
 from collections.abc import Callable
 from typing import Union, Optional
 
-class InvalidStringLexerException(Exception):
-    def __init__(self, filepath: str, page: int, line: int, col: int, string: str):
-        super().__init__("{}:{}:{}:{}: {}: '{}'.".format(filepath, page, line, col, self.__class__.__name__, string))
+class InvalidCharacterLexerError(Exception):
+    def __init__(self, filepath: str, line: int, col: int, char: str):
+        super().__init__("{}:{}:{}: {}: '{}'.".format(filepath, line, col, self.__class__.__name__, char))
 
 class _TerminalMeta(type):
     def __str__(self) -> str:
@@ -35,11 +35,9 @@ class _TerminalMeta(type):
         return len(str(self))
 
 class Terminal(metaclass = _TerminalMeta):
-    def __init__(self, page: int, line: int, col: int):
-        self.page: int = page
+    def __init__(self, line: int, col: int):
         self.line: int = line
         self.col: int = col
-        self.end_page: int = page
         self.end_line: int = line
         self.end_col: int = col + len(self)
 
@@ -59,18 +57,21 @@ class Terminal(metaclass = _TerminalMeta):
         return other == self.__class__ or isinstance(other, self.__class__)
 
 class Identifier(Terminal):
-    def __init__(self, page: int, line: int, col: int, value: str):
+    def __init__(self, line: int, col: int, value: str):
         self.value: str = value
-        super().__init__(page, line, col)
+        super().__init__(line, col)
         self.str: str = "Identifier<{}>".format(value)
 
     def __len__(self) -> int:
         return len(self.value)
 
 class IgnoreToken(Terminal):
-    def __init__(self, page: int, line: int, col: int, value: str):
-        self.str: str = value
-        super().__init__(page, line, col)
+    def __init__(self, line: int, col: int, value: str):
+        self.value: str = value
+        super().__init__(line, col)
+
+    def __len__(self) -> int:
+        return len(self.value)
 
 class EOI(Terminal):
     str: str = "end of input"
@@ -78,21 +79,28 @@ class EOI(Terminal):
     def __len__(self) -> int:
         return 0
 
-def process_non_printable(input: str, position: int, page: int, line: int, col: int) -> Optional[IgnoreToken]:
-    token: IgnoreToken = IgnoreToken(page, line, col, input[position])
+def process_Identifier(filepath: str, input: str, position: int, line: int, col: int) -> Optional[Identifier]:
+    if input[position].isalpha() or input[position] == "_":
+        end: int = position + 1
+
+        while end < len(input) and (input[end].isalnum() or input[end] == "_"):
+            end += 1
+
+        return Identifier(line, col, input[position:end])
+
+    return None
+
+def process_non_printable(input: str, position: int, line: int, col: int) -> Optional[IgnoreToken]:
+    token: IgnoreToken = IgnoreToken(line, col, input[position])
 
     if input[position] == "\t":
         token.end_col = col + 8 - (col + 7) % 8
     elif input[position] == "\n":
         token.end_line = line + 1
         token.end_col = 1
-    elif input[position] == "\v":
+    elif input[position] in {"\v", "\f"}:
         token.end_line = line + 1
         token.end_col = col
-    elif input[position] == "\f":
-        token.end_page = page + 1
-        token.end_line = 1
-        token.end_col = 1
     elif input[position] == " ":
         pass
     else:
@@ -100,71 +108,64 @@ def process_non_printable(input: str, position: int, page: int, line: int, col: 
 
     return token
 
-def lexer_run(filepath: str, input: str, terminals: list[Union[type[Terminal], tuple[str, Callable[[str, str, int, int, int, int], Terminal]]]], id_chars: str = "_") -> Optional[list[Terminal]]:
+def lexer_run(filepath: str, input: str, terminals: set[Union[type[Terminal], tuple[str, Callable[[str, str, int, int, int], Terminal]]]], default_handler: Callable[[str, str, int, int, int], Optional[Terminal]] = process_Identifier) -> list[Terminal]:
     input = input.replace("\r\n", "\n")
     input = input.replace("\n\r", "\n")
     input = input.replace("\r", "\n")
-    terminals = sorted(terminals, key = lambda terminal: (str(terminal) if isinstance(terminal, _TerminalMeta) else terminal[0]) + "\U0010FFFF")
+    terminals_sorted: list[Union[type[Terminal], tuple[str, Callable[[str, str, int, int, int], Terminal]]]] = sorted(terminals, key = lambda terminal: (str(terminal) if isinstance(terminal, _TerminalMeta) else terminal[0]) + "\U0010FFFF")
     tokens: list[Terminal] = []
-    page: int = 1
     line: int = 1
     col: int = 1
     position: int = 0
 
     while position < len(input):
-        try:
-            token_added: bool = False
+        token_added: bool = False
 
-            for terminal in terminals:
-                if isinstance(terminal, _TerminalMeta):
-                    if input[position:position + len(terminal)] == str(terminal):
-                        if (not str(terminal)[-1].isalnum() and str(terminal)[-1] not in id_chars) or position + len(terminal) >= len(input) or (not input[position + len(terminal)].isalnum() and input[position + len(terminal)] not in id_chars):
-                            tokens.append(terminal(page, line, col))
-                            position += len(tokens[-1])
-                            col += len(tokens[-1])
-                            token_added = True
+        for terminal in terminals_sorted:
+            if isinstance(terminal, _TerminalMeta):
+                if input[position:position + len(terminal)] == str(terminal):
+                    if (not str(terminal)[-1].isalnum() and str(terminal)[-1] not in id_chars) or position + len(terminal) >= len(input) or (not input[position + len(terminal)].isalnum() and input[position + len(terminal)] not in id_chars):
+                        tokens.append(terminal(line, col))
+                        position += len(tokens[-1])
+                        line = tokens[-1].end_line
+                        col = tokens[-1].end_col
+                        token_added = True
 
-                        break
-                    elif input[position:position + len(terminal)] + "\U0010FFFF" < str(terminal) + "\U0010FFFF":
-                        break
-                elif input[position:position + len(terminal[0])] == terminal[0]:
-                    token: Terminal = terminal[1](filepath, input, position, page, line, col)
+                    break
+                elif input[position:position + len(terminal)] + "\U0010FFFF" < str(terminal) + "\U0010FFFF":
+                    break
+            elif input[position:position + len(terminal[0])] == terminal[0]:
+                token: Terminal = terminal[1](filepath, input, position, line, col)
 
-                    if not isinstance(token, IgnoreToken):
-                        tokens.append(token)
+                if not isinstance(token, IgnoreToken):
+                    tokens.append(token)
 
-                    position += len(token)
-                    page = token.end_page
+                position += len(token)
+                line = token.end_line
+                col = token.end_col
+                token_added = True
+                break
+            elif input[position:position + len(terminal[0])] + "\U0010FFFF" < terminal[0] + "\U0010FFFF":
+                break
+
+        if not token_added:
+            token: Optional[Terminal] = default_handler(filepath, input, position, line, col)
+
+            if token != None:
+                if not isinstance(token, IgnoreToken):
+                    tokens.append(token)
+
+                position += len(token)
+                line = token.end_line
+                col = token.end_col
+            elif not input[position].isprintable() or input[position] == " ":
+                token = process_non_printable(input, position, line, col)
+                position += 1
+
+                if token != None:
                     line = token.end_line
                     col = token.end_col
-                    token_added = True
-                    break
-                elif input[position:position + len(terminal[0])] + "\U0010FFFF" < terminal[0] + "\U0010FFFF":
-                    break
-
-            if not token_added:
-                if input[position].isalpha() or input[position] in id_chars:
-                    end: int = position + 1
-
-                    while end < len(input) and (input[end].isalnum() or input[end] in id_chars):
-                        end += 1
-
-                    tokens.append(Identifier(page, line, col, input[position:end]))
-                    position += len(tokens[-1])
-                    col += len(tokens[-1])
-                elif not input[position].isprintable() or input[position] == " ":
-                    token: Optional[IgnoreToken] = process_non_printable(input, position, page, line, col)
-
-                    if token != None:
-                        page = token.end_page
-                        line = token.end_line
-                        col = token.end_col
-
-                    position += 1
-                else:
-                    raise InvalidStringLexerException(filepath, page, line, col, input[position:])
-        except InvalidStringLexerException as e:
-            print(e)
-            return None
+            else:
+                raise InvalidCharacterLexerError(filepath, line, col, input[position])
 
     return tokens
