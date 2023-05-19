764e9ac7634678bbd4a25b3ce6a80357a017c1fc
Natan Junges <natanajunges@gmail.com>
2023-05-19 11:34:37 -0300

    Revert naming NonTerminal to Production

--- alchemist/front/parser/__init__.py
+++ alchemist/front/parser/__init__.py
@@ -16,56 +16,56 @@
 
 from typing import Optional, cast, TYPE_CHECKING
 
-from .. import ASTNode, CompilerError
+from .. import CompilerError
 from ..lexer import CompilerEOIError
 
 if TYPE_CHECKING:
     from ..lexer import Lexer, Terminal
 
 
-class NonTerminal(ASTNode):  # pylint: disable=R0903
+class Production:  # pylint: disable=R0903
     _left_recursive: bool = True
 
     @staticmethod
     def process_left_recursion(
-        parent: Optional["NonTerminal"], parser: "Parser", path: "Terminal", nonterminal: "NonTerminal"
-    ) -> tuple["NonTerminal", set["Terminal"]]:
+        parent: Optional["Production"], parser: "Parser", path: "Terminal", production: "Production"
+    ) -> tuple["Production", set["Terminal"]]:
         nextpaths: set["Terminal"] = set()
-        recursive_path: set[type[NonTerminal]] = cast(set[type[NonTerminal]], nonterminal.recursive_path)
-        nonterminal.recursive_path = None
+        recursive_path: set[type[Production]] = cast(set[type[Production]], production.recursive_path)
+        production.recursive_path = None
 
         while True:
-            if nextpaths | nonterminal.output_paths == nextpaths:
+            if nextpaths | production.output_paths == nextpaths:
                 break
 
             for prod in recursive_path:
-                del parser.nonterminals[path, prod]
+                del parser.productions[path, prod]
 
-            nextpaths |= nonterminal.output_paths
-            parser.nonterminals[path, nonterminal.__class__] = nonterminal
+            nextpaths |= production.output_paths
+            parser.productions[path, production.__class__] = production
             parser.lexer.set_state(path)
 
             try:
-                nonterminal = nonterminal.__class__(parent, parser)
+                production = production.__class__(parent, parser)
             except CompilerError:
                 break
 
-        return nonterminal, nextpaths
+        return production, nextpaths
 
-    def __init__(self, parent: Optional["NonTerminal"], parser: "Parser") -> None:
-        self.parent: NonTerminal | None = parent
+    def __init__(self, parent: Optional["Production"], parser: "Parser") -> None:
+        self.parent: Production | None = parent
         self.parser: Parser = parser
         self.output_paths: set["Terminal"] = set()
         self.input_path: Optional["Terminal"] = parser.lexer.get_state()
-        self.recursive_path: set[type[NonTerminal]] | None = None
+        self.recursive_path: set[type[Production]] | None = None
         self._derive()
         self.parent = None
         self.input_path = None
 
-    def __left_recursive(self, paths: set["Terminal"], node: type["NonTerminal"]) -> bool:
-        if self.input_path in paths and (self.input_path, node) not in self.parser.nonterminals:
-            parent: NonTerminal | None = self
-            recursive_path: set[type[NonTerminal]] = set()
+    def __left_recursive(self, paths: set["Terminal"], node: type["Production"]) -> bool:
+        if self.input_path in paths and (self.input_path, node) not in self.parser.productions:
+            parent: Production | None = self
+            recursive_path: set[type[Production]] = set()
 
             while parent is not None and parent.input_path is self.input_path:
                 if parent.__class__ is node:
@@ -81,32 +81,32 @@ class NonTerminal(ASTNode):  # pylint: disable=R0903
 
         return False
 
-    def _process_paths(self, paths: set["Terminal"], node: type[ASTNode]) -> set["Terminal"]:
+    def _process_paths(self, paths: set["Terminal"], node: type["Terminal"] | type["Production"]) -> set["Terminal"]:
         nextpaths: set["Terminal"] = set()
         state: "Terminal" = self.parser.lexer.get_state()
 
-        if issubclass(node, NonTerminal):
+        if issubclass(node, Production):
             left_recursive: bool = self._left_recursive and self.__left_recursive(paths, node)
 
             for path in paths:
-                if (path, node) not in self.parser.nonterminals:
+                if (path, node) not in self.parser.productions:
                     if left_recursive and path is self.input_path:
                         continue
 
                     self.parser.lexer.set_state(path)
 
                     try:
-                        nonterminal: NonTerminal = node(self, self.parser)
+                        production: Production = node(self, self.parser)
 
-                        if nonterminal.recursive_path is not None:
-                            nextpaths |= self.process_left_recursion(self, self.parser, path, nonterminal)[1]
+                        if production.recursive_path is not None:
+                            nextpaths |= self.process_left_recursion(self, self.parser, path, production)[1]
                         else:
-                            nextpaths |= nonterminal.output_paths
-                            self.parser.nonterminals[path, node] = nonterminal
+                            nextpaths |= production.output_paths
+                            self.parser.productions[path, node] = production
                     except CompilerSyntaxError:
-                        self.parser.nonterminals[path, node] = None
-                elif self.parser.nonterminals[path, node] is not None:
-                    nextpaths |= cast(NonTerminal, self.parser.nonterminals[path, node]).output_paths
+                        self.parser.productions[path, node] = None
+                elif self.parser.productions[path, node] is not None:
+                    nextpaths |= cast(Production, self.parser.productions[path, node]).output_paths
         else:  # Terminal
             for path in paths:
                 self.parser.lexer.set_state(path)
@@ -130,23 +130,23 @@ class NonTerminal(ASTNode):  # pylint: disable=R0903
 
 
 class Parser:  # pylint: disable=R0903
-    _start: type[NonTerminal] = NonTerminal
+    _start: type[Production] = Production
 
     def __init__(self, lexer: "Lexer") -> None:
         self.lexer: "Lexer" = lexer
-        self.nonterminals: dict[tuple["Terminal", type[NonTerminal]], NonTerminal | None] = {}
+        self.productions: dict[tuple["Terminal", type[Production]], Production | None] = {}
 
-    def parse(self) -> NonTerminal | None:
+    def parse(self) -> Production | None:
         path: "Terminal" = self.lexer.get_state()
 
         try:
-            ast: NonTerminal = self._start(None, self)
+            ast: Production = self._start(None, self)
         except CompilerError as error:
             print(error)
             return None
 
         if ast.recursive_path is not None:
-            ast = NonTerminal.process_left_recursion(None, self, path, ast)[0]
+            ast = Production.process_left_recursion(None, self, path, ast)[0]
 
         output_path: "Terminal" | None = None
 
@@ -164,18 +164,18 @@ class Parser:  # pylint: disable=R0903
 
 
 class CompilerSyntaxError(CompilerError):
-    def __init__(self, nonterminal: NonTerminal, msg: str) -> None:
-        super().__init__(nonterminal.parser.lexer.input, "Syntax Error", f"In {nonterminal.__class__.__name__}: {msg}")
+    def __init__(self, production: Production, msg: str) -> None:
+        super().__init__(production.parser.lexer.input, "Syntax Error", f"In {production.__class__.__name__}: {msg}")
 
 
 class CompilerNoPathError(CompilerSyntaxError):
-    def __init__(self, nonterminal: NonTerminal) -> None:
-        super().__init__(nonterminal, "Could not find a parsing path.")
+    def __init__(self, production: Production) -> None:
+        super().__init__(production, "Could not find a parsing path.")
 
 
 class CompilerNEOIError(CompilerSyntaxError):
-    def __init__(self, nonterminal: NonTerminal) -> None:
-        super().__init__(nonterminal, "Expected end of input.")
+    def __init__(self, production: Production) -> None:
+        super().__init__(production, "Expected end of input.")
 
 
 class BreakException(Exception):
