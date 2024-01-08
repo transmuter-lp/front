7e651ee6d5bffd237c3adee29b5a228be8619763
natanjunges <natanajunges@gmail.com>
2024-01-08 17:53:03 -0300

    Fix copyright lines

--- src/alchemist/front/aether/common.py
+++ src/alchemist/front/aether/common.py
@@ -1,5 +1,5 @@
 # Alchemist front-end, front-end libraries and utilities for the Alchemist compiler infrastructure
-# Copyright (C) 2021, 2023, 2024  Natan Junges <natanajunges@gmail.com>
+# Copyright (C) 2024  Natan Junges <natanajunges@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
