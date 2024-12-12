837c32f3303c5560cf9ab6f8516a37025f61b26d
Natan Junges <natanajunges@gmail.com>
2024-12-12 15:12:16 -0300

    Implement backend for lexical file

--- src/transmuter/front/aether/back/common.py
+++ src/transmuter/front/aether/back/common.py
@@ -15,10 +15,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
-from ....semantic.common import TransmuterNonterminalTreeNode
-from ....semantic.symbol_table import TransmuterSymbolTable
+from ...semantic.common import (
+    TransmuterTerminalTreeNode,
+    TransmuterNonterminalTreeNode,
+    TransmuterTreeFold,
+)
+from ...semantic.symbol_table import TransmuterSymbolTable
+from ..lexical import (
+    CommercialAt,
+    LeftParenthesis,
+    RightParenthesis,
+    DoubleVerticalLine,
+    DoubleAmpersand,
+    ExclamationMark,
+)
+from ..syntactic import (
+    DisjunctionCondition,
+    ConjunctionCondition,
+    NegationCondition,
+    PrimitiveCondition,
+)
+from ..semantic import LexicalState, LexicalSymbol
 
 
 @dataclass
@@ -27,7 +46,9 @@ class AetherFileFold:
 
     @staticmethod
     def indent(value: str) -> str:
-        return f"    {value.replace('\n', '\n    ')}"
+        return f"    {value.replace('\n', '\n    ')}".replace(
+            "    \n", "\n"
+        ).rstrip()
 
     def fold(self) -> str:
         raise NotImplementedError()
@@ -46,4 +67,217 @@ class AetherCommonFileFold(AetherFileFold):
         raise NotImplementedError()
 
     def fold_condition(self, name: str) -> str:
+        raise NotImplementedError()
+
+
+class AetherConditionFold(TransmuterTreeFold[str]):
+    def fold_internal(
+        self, node: TransmuterNonterminalTreeNode, children: list[str]
+    ) -> str | None:
+        if len(children) == 0:
+            return None
+
+        if len(children) == 1:
+            if node.type_ == NegationCondition and len(node.children) % 2 == 0:
+                return self.fold_negation(node, children[0])
+
+            if node.type_ == PrimitiveCondition:
+                return self.fold_primitive(node, children[0])
+
+            return children[0]
+
+        if node.type_ == DisjunctionCondition:
+            return self.fold_disjunction(node, children)
+
+        assert node.type_ == ConjunctionCondition
+        return self.fold_conjunction(node, children)
+
+    def fold_external(self, node: TransmuterTerminalTreeNode) -> str | None:
+        if node.type_ in (
+            CommercialAt,
+            DoubleVerticalLine,
+            DoubleAmpersand,
+            ExclamationMark,
+            LeftParenthesis,
+            RightParenthesis,
+        ):
+            return None
+
+        return node.end_terminal.value
+
+    def fold_disjunction(
+        self, node: TransmuterNonterminalTreeNode, children: list[str]
+    ) -> str:
+        raise NotImplementedError()
+
+    def fold_conjunction(
+        self, node: TransmuterNonterminalTreeNode, children: list[str]
+    ) -> str:
+        raise NotImplementedError()
+
+    def fold_negation(
+        self, node: TransmuterNonterminalTreeNode, child: str
+    ) -> str:
+        raise NotImplementedError()
+
+    def fold_primitive(
+        self, node: TransmuterNonterminalTreeNode, child: str
+    ) -> str:
+        raise NotImplementedError()
+
+
+@dataclass
+class AetherLexicalFileFold(AetherFileFold):
+    condition_fold_type: type[AetherConditionFold]
+    _condition_fold: AetherConditionFold | None = field(
+        default=None, init=False, repr=False
+    )
+
+    def fold(self) -> str:
+        terminal_tag_names = []
+        terminal_tags = []
+
+        for name, symbol in self.symbol_table:
+            assert isinstance(symbol, LexicalSymbol)
+            terminal_tag_names.append(name)
+            states_start = (
+                self.fold_states_start(symbol.states_start)
+                if symbol.states_start != [0]
+                else None
+            )
+            start = (
+                self.fold_start(self.fold_condition(symbol.start))
+                if symbol.start is not None
+                else None
+            )
+            ignore = (
+                self.fold_ignore(
+                    self.fold_condition(symbol.ignore)
+                    if symbol.ignore is not True
+                    else None
+                )
+                if symbol.ignore is not False
+                else None
+            )
+
+            if (
+                len(symbol.static_positives) > 0
+                or len(symbol.conditional_positives) > 0
+            ):
+                static_positives = self.fold_static_positives(
+                    symbol.static_positives
+                )
+                conditional_positives = [
+                    self.fold_conditional_positive(
+                        p, self.fold_condition(symbol.conditional_positives[p])
+                    )
+                    for p in symbol.conditional_positives
+                ]
+                positives = self.fold_positives(
+                    static_positives, conditional_positives
+                )
+            else:
+                positives = None
+
+            if (
+                len(symbol.static_negatives) > 0
+                or len(symbol.conditional_negatives) > 0
+            ):
+                static_negatives = self.fold_static_negatives(
+                    symbol.static_negatives
+                )
+                conditional_negatives = [
+                    self.fold_conditional_negative(
+                        n, self.fold_condition(symbol.conditional_negatives[n])
+                    )
+                    for n in symbol.conditional_negatives
+                ]
+                negatives = self.fold_negatives(
+                    static_negatives, conditional_negatives
+                )
+            else:
+                negatives = None
+
+            states = [
+                self.fold_state(i, symbol.states[i])
+                for i in range(len(symbol.states))
+            ]
+            nfa = self.fold_nfa(states)
+            terminal_tags.append(
+                self.fold_terminal_tag(
+                    name,
+                    states_start,
+                    start,
+                    ignore,
+                    positives,
+                    negatives,
+                    nfa,
+                )
+            )
+
+        return self.fold_file(terminal_tag_names, terminal_tags)
+
+    def fold_condition(self, value: TransmuterNonterminalTreeNode) -> str:
+        if self._condition_fold is None:
+            self._condition_fold = self.condition_fold_type(value)
+        else:
+            self._condition_fold.tree = value
+
+        self._condition_fold.visit()
+        assert len(self._condition_fold.fold_queue) > 0
+        assert self._condition_fold.fold_queue[0] is not None
+        return self._condition_fold.fold_queue[0]
+
+    def fold_file(
+        self, terminal_tag_names: list[str], terminal_tags: list[str]
+    ) -> str:
+        raise NotImplementedError()
+
+    def fold_terminal_tag(
+        self,
+        name: str,
+        states_start: str | None,
+        start: str | None,
+        ignore: str | None,
+        positives: str | None,
+        negatives: str | None,
+        nfa: str,
+    ) -> str:
+        raise NotImplementedError()
+
+    def fold_states_start(self, value: list[int]) -> str:
+        raise NotImplementedError()
+
+    def fold_start(self, value: str) -> str:
+        raise NotImplementedError()
+
+    def fold_ignore(self, value: str | None) -> str:
+        raise NotImplementedError()
+
+    def fold_positives(
+        self, static_positives: str, conditional_positives: list[str]
+    ) -> str:
+        raise NotImplementedError()
+
+    def fold_negatives(
+        self, static_negatives: str, conditional_negatives: list[str]
+    ) -> str:
+        raise NotImplementedError()
+
+    def fold_nfa(self, states: list[str]) -> str:
+        raise NotImplementedError()
+
+    def fold_static_positives(self, value: list[str]) -> str:
+        raise NotImplementedError()
+
+    def fold_conditional_positive(self, value: str, condition: str) -> str:
+        raise NotImplementedError()
+
+    def fold_static_negatives(self, value: list[str]) -> str:
+        raise NotImplementedError()
+
+    def fold_conditional_negative(self, value: str, condition: str) -> str:
+        raise NotImplementedError()
+
+    def fold_state(self, index: int, value: LexicalState) -> str:
         raise NotImplementedError()
--- src/transmuter/front/aether/back/python.py
+++ src/transmuter/front/aether/back/python.py
@@ -17,7 +17,19 @@
 
 import builtins
 
-from .common import AetherCommonFileFold
+from ...semantic.common import TransmuterNonterminalTreeNode
+from ..semantic import (
+    LexicalSimplePattern,
+    LexicalWildcardPattern,
+    LexicalRangePattern,
+    LexicalBracketPattern,
+    LexicalState,
+)
+from .common import (
+    AetherCommonFileFold,
+    AetherConditionFold,
+    AetherLexicalFileFold,
+)
 
 
 def _escape_identifier(value: str) -> str:
@@ -83,13 +95,221 @@ def _escape_identifier(value: str) -> str:
     return value
 
 
+def _escape_char(value: str) -> str:
+    if value == '"':
+        return '\\"'
+
+    if len(value) == 2 and value[0] == "\\" and value[1] in " $()*+.;?[^{|":
+        return value[1]
+
+    return value
+
+
 class CommonFileFold(AetherCommonFileFold):
     def fold_file(self, conditions: list[str]) -> str:
-        return f"""from transmuter.front.common import TransmuterConditions, TransmuterCondition
-
-
-class Conditions(TransmuterConditions):
-    {'\n    '.join(conditions)}"""
+        return f"from transmuter.front.common import TransmuterConditions, TransmuterCondition\n\n\nclass Conditions(TransmuterConditions):\n    {'\n    '.join(conditions)}"
 
     def fold_condition(self, name: str) -> str:
         return f"{_escape_identifier(name)} = TransmuterCondition()"
+
+
+class ConditionFold(AetherConditionFold):
+    def fold_disjunction(
+        self, node: TransmuterNonterminalTreeNode, children: list[str]
+    ) -> str:
+        return " or ".join(children)
+
+    def fold_conjunction(
+        self, node: TransmuterNonterminalTreeNode, children: list[str]
+    ) -> str:
+        return " and ".join(children)
+
+    def fold_negation(
+        self, node: TransmuterNonterminalTreeNode, child: str
+    ) -> str:
+        assert len(node.children) > 0
+        assert isinstance(node.children[-1], TransmuterNonterminalTreeNode)
+
+        if len(node.children[-1].children) == 1:
+            return child.replace(" in ", " not in ", 1)
+
+        return f"not {child}"
+
+    def fold_primitive(
+        self, node: TransmuterNonterminalTreeNode, child: str
+    ) -> str:
+        if len(node.children) == 1:
+            return f"Conditions.{_escape_identifier(child)} in conditions"
+
+        return f"({child})"
+
+
+class LexicalFileFold(AetherLexicalFileFold):
+    def fold_file(
+        self, terminal_tag_names: list[str], terminal_tags: list[str]
+    ) -> str:
+        return f"from transmuter.front.lexical import TransmuterTerminalTag, TransmuterLexer\nfrom .common import Conditions\n\n\n{'\n\n\n'.join(terminal_tags)}\n\n\nclass Lexer(TransmuterLexer):\n    TERMINAL_TAGS = [{', '.join(_escape_identifier(t) for t in terminal_tag_names)}]"
+
+    def fold_terminal_tag(
+        self,
+        name: str,
+        states_start: str | None,
+        start: str | None,
+        ignore: str | None,
+        positives: str | None,
+        negatives: str | None,
+        nfa: str,
+    ) -> str:
+        terminal_tag = (
+            f"class {_escape_identifier(name)}(TransmuterTerminalTag):\n"
+        )
+
+        if states_start is not None:
+            terminal_tag += f"    {states_start}\n\n"
+
+        if start is not None:
+            terminal_tag += f"{self.indent(start)}\n\n"
+
+        if ignore is not None:
+            terminal_tag += f"{self.indent(ignore)}\n\n"
+
+        if positives is not None:
+            terminal_tag += f"{self.indent(positives)}\n\n"
+
+        if negatives is not None:
+            terminal_tag += f"{self.indent(negatives)}\n\n"
+
+        terminal_tag += f"{self.indent(nfa)}"
+        return terminal_tag
+
+    def fold_states_start(self, value: list[int]) -> str:
+        return f"STATES_START = {' | '.join(f'1 << {s}' for s in value)}"
+
+    def fold_start(self, value: str) -> str:
+        return f"@staticmethod\ndef start(conditions):\n    return {value}"
+
+    def fold_ignore(self, value: str | None) -> str:
+        return f"@staticmethod\ndef ignore(conditions):\n    return {value if value is not None else 'True'}"
+
+    def fold_positives(
+        self, static_positives: str, conditional_positives: list[str]
+    ) -> str:
+        positives = f"@staticmethod\ndef positives(conditions):\n    {static_positives}\n"
+
+        if len(conditional_positives) > 0:
+            positives += (
+                f"\n{self.indent('\n\n'.join(conditional_positives))}\n\n"
+            )
+
+        positives += "    return positives"
+        return positives
+
+    def fold_negatives(
+        self, static_negatives: str, conditional_negatives: list[str]
+    ) -> str:
+        negatives = f"@staticmethod\ndef negatives(conditions):\n    {static_negatives}\n"
+
+        if len(conditional_negatives) > 0:
+            negatives += (
+                f"\n{self.indent('\n\n'.join(conditional_negatives))}\n\n"
+            )
+
+        negatives += "    return negatives"
+        return negatives
+
+    def fold_nfa(self, states: list[str]) -> str:
+        return f"@staticmethod\ndef nfa(current_states, char):\n    state_accept = False\n    next_states = 0\n\n{self.indent('\n\n'.join(states))}\n\n    return state_accept, next_states"
+
+    def fold_static_positives(self, value: list[str]) -> str:
+        if len(value) == 0:
+            return "positives = set()"
+
+        return f"positives = {{{', '.join(_escape_identifier(p) for p in value)}}}"
+
+    def fold_conditional_positive(self, value: str, condition: str) -> str:
+        return (
+            f"if {condition}:\n    positives.add({_escape_identifier(value)})"
+        )
+
+    def fold_static_negatives(self, value: list[str]) -> str:
+        if len(value) == 0:
+            return "negatives = set()"
+
+        return f"negatives = {{{', '.join(_escape_identifier(p) for p in value)}}}"
+
+    def fold_conditional_negative(self, value: str, condition: str) -> str:
+        return (
+            f"if {condition}:\n    negatives.add({_escape_identifier(value)})"
+        )
+
+    def fold_state(self, index: int, value: LexicalState) -> str:
+        state = f"if 1 << {index} & current_states{' and ' if not isinstance(value.pattern, LexicalWildcardPattern) else ''}"
+
+        if isinstance(value.pattern, LexicalSimplePattern):
+            state += f'char == "{_escape_char(value.pattern.char)}"'
+        elif isinstance(value.pattern, LexicalBracketPattern):
+            patterns = []
+            i = 0
+
+            while i < len(value.pattern.patterns):
+                pattern = value.pattern.patterns[i]
+
+                if isinstance(pattern, LexicalSimplePattern):
+                    if i + 1 < len(value.pattern.patterns) and isinstance(
+                        value.pattern.patterns[i + 1], LexicalSimplePattern
+                    ):
+                        patterns.append(
+                            f'char in "{_escape_char(pattern.char)}{_escape_char(value.pattern.patterns[i + 1].char)}'
+                        )
+                        j = i + 2
+
+                        while j < len(value.pattern.patterns) and isinstance(
+                            value.pattern.patterns[j], LexicalSimplePattern
+                        ):
+                            patterns[-1] += _escape_char(
+                                value.pattern.patterns[j].char
+                            )
+                            j += 1
+
+                        patterns[-1] += '"'
+                        i = j
+                        continue
+
+                    patterns.append(f'char == "{_escape_char(pattern.char)}"')
+                else:
+                    assert isinstance(pattern, LexicalRangePattern)
+                    patterns.append(
+                        f'"{_escape_char(pattern.first_char)}" <= char <= "{_escape_char(pattern.last_char)}"'
+                    )
+
+                i += 1
+
+            if len(patterns) > 1:
+                if value.pattern.negative_match:
+                    state += "not "
+
+                state += f"({' or '.join(patterns)})"
+            else:
+                assert len(patterns) == 1
+
+                if value.pattern.negative_match:
+                    if patterns[0].startswith("char in"):
+                        state += patterns[0].replace(
+                            "char in", "char not in", 1
+                        )
+                    elif patterns[0].startswith("char =="):
+                        state += patterns[0].replace("char ==", "char !=", 1)
+                    else:
+                        state += f"not {patterns[0]}"
+                else:
+                    state += patterns[0]
+
+        state += ":"
+
+        if value.state_accept:
+            state += "\n    state_accept = True"
+
+        if len(value.next_states_indexes) > 0:
+            state += f"\n    next_states |= {' | '.join(f'1 << {n}' for n in value.next_states_indexes)}"
+
+        return state
